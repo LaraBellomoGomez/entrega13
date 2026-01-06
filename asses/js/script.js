3f4b25239ccaeede51e416b75523bde15bafd3da
Lara BG <larabellomogomez@gmail.com>
2026-01-06 20:52:53 +0100

    primer ejercicio

--- asses/js/script.js
+++ asses/js/script.js
@@ -1,35 +1,11 @@
-precio = 400000
+const imagen = document.querySelector(".img");
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
-const total = document.querySelector(".valor-total");
-
-
-const cantidad = document.querySelector(".cantidad");
-const mas = document.querySelector(".mas");
-const menos = document.querySelector(".menos");
-
-
-
-
-function actualizarTotal() {
-    let cantidadActual = Number(cantidad.innerHTML);
-    let totalPrecio = precio * cantidadActual;
-    total.innerHTML = totalPrecio;
-}
-
-mas.addEventListener("click", () => {
-    let contador = Number(cantidad.innerHTML);
-    contador += 1;
-    cantidad.innerHTML = contador;
-    actualizarTotal(); 
+imagen.addEventListener("click", () => {
+  if (imagen.style.border) {
+    imagen.style.border = "";
+  } else {
+    imagen.style.border = "2px solid red";
+  }
 });
 
-menos.addEventListener("click", () => {
-    let contador = Number(cantidad.innerHTML);
-    if (contador > 0) {
-        contador -= 1;
-        cantidad.innerHTML = contador;
-        actualizarTotal(); 
-    }
-});
+
